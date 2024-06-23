cd283123c4d63f1201b3a8d06b4f59b146c4c298
paulalextech <psteane123@hotmail.com>
2024-06-23 09:37:26 +0100

    function on game start button

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const game_container = document.getElementById('game_container');
 const timeEl = document.getElementById('time');
 const scoreEl = document.getElementById('score');
 const message = document.getElementById('message');
+let seconds = 0;
+let score = 0;
+let selected_insect = {};
+
+start_btn.addEventListener('onClick', () => screens[0].classList.add('up'));
