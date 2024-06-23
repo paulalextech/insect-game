b4507e7d1caa8d7f0a5c2f7eb08bfc5487dcde91
paulalextech <psteane123@hotmail.com>
2024-06-23 09:33:10 +0100

    set all variables

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const screens = document.querySelectorAll('.screen');
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
+const start_btn = document.getElementById('start-btn');
+const game_container = document.getElementById('game_container');
+const timeEl = document.getElementById('time');
+const scoreEl = document.getElementById('score');
+const message = document.getElementById('message');
